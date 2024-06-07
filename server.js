b7b745919118960b8d0c76857c665696ab030786
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-06-07 19:38:37 +0330

    mongodb connection

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const http = require('http');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
-
-
+const { mongoConnect } = require('./services/mongo');
+require('dotenv').config();
+const server = http.createServer(app);
 
 app.use(express.json());
 
 
 
-app.listen(3000, () => console.log('Server Started'));
+async function startServer() {
+   await mongoConnect();
+   server.listen(3000, () => {
+     console.log('Listening on port 3000');
+   });
+ }
+ 
+ startServer();
